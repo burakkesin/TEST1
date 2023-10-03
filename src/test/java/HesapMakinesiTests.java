215fe053524b84d469f44079a42f0db3b663fd73
burak <burakkesin@hotmail.com.tr>
2023-10-03 18:54:41 +0300

    second commit

--- src/test/java/HesapMakinesiTests.java
+++ src/test/java/HesapMakinesiTests.java
@@ -1,28 +1,52 @@
 
 import org.example.HesapMakinesi;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 public class HesapMakinesiTests {
 
+
+    @BeforeAll
+     public static void setupClass(){
+        System.out.println("setup class metodu calıştırıldı");
+    }
+
+    HesapMakinesi hesapMakinesi;
+
+    @BeforeEach
+    public void setUp() {
+        hesapMakinesi =new HesapMakinesi();
+        System.out.println("setup metodu calışıyor");
+
+    }
+
     @Test
     public void toplamaTesti(){
-        HesapMakinesi hesapMakinesi = new HesapMakinesi();
         double toplamaSonucu = hesapMakinesi.toplama(1.5,3.6);
         Assertions.assertEquals(5.1,toplamaSonucu);
 
     }
+    @Disabled("sonra kosacak")
     @Test
     public void cıkarmaTesti(){
-        HesapMakinesi hesapMakinesi = new HesapMakinesi();
         double cıkarmaSonucu = hesapMakinesi.cıkarma(5,3);
         Assertions.assertEquals(3,cıkarmaSonucu);
     }
 
     @Test
     public void carpmaTesti(){
-        HesapMakinesi hesapMakinesi = new HesapMakinesi();
         double carpmaSonucu = hesapMakinesi.carpma(8,9);
         Assertions.assertEquals(72,carpmaSonucu);
     }
+
+    @AfterEach
+    public void tearDown(){
+        System.out.println("Tear down çalıştı");
+    }
+
+    @AfterAll
+
+    public static void tearDownClass(){
+        System.out.println("Tear down class çalıtı");
+    }
+
 }
